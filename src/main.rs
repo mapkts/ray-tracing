4c46ce2afb12d6a3ff8518788babaf4feae48264
Aaron Taner <mapkts@gmail.com>
2021-04-11 00:05:33 +0800

    dev: add progress

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use std::io;
+use std::io::prelude::*;
 fn main() {
     let image_width = 256;
     let image_height = 256;
 
     println!("P3\n{} {}\n255", image_height, image_width);
 
+    let stdout = io::stdout();
+    let mut lock = stdout.lock();
     for h in (0..image_height).rev() {
+        write!(io::stderr(), "\rScanlines remaining: {}  ", h).expect("error writing stderr");
         for w in 0..image_width {
             let r = w as f64 / (image_width - 1) as f64;
             let g = h as f64 / (image_height - 1) as f64;
@@ -14,7 +19,8 @@ fn main() {
             let ig = (255.999 * g) as i32;
             let ib = (255.999 * b) as i32;
 
-            println!("{} {} {}", ir, ig, ib);
+            write!(lock, "{} {} {}", ir, ig, ib).expect("error writing stdout");
         }
     }
+    drop(lock);
 }
